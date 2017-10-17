c9b79aaa6473871de4936149c3ef8fef58a85d5a
Courtney Myers <courtney.aaron.myers@gmail.com>
2017-10-16 22:16:12 -0400

    Split webpack config for dev and prod Add webpack-merge package. Split webpack config into common, dev, and prod. Add sourcemaps to prod build. Remove unused 'watch' npm script

--- site/web/app/themes/gsw/build/webpack.config.js
+++ site/web/app/themes/gsw/build/webpack.config.js
@@ -1,11 +1,15 @@
 const path = require('path');
 const webpack = require('webpack');
+const merge = require('webpack-merge');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ExtractTextWebpackPlugin = require('extract-text-webpack-plugin');
 const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 
-module.exports = {
+const TARGET = process.env.npm_lifecycle_event;
+
+// common webpack config
+const common = {
   entry: {
     alpha: './src/alpha.js',
     beta: './src/beta.js',
@@ -16,32 +20,16 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
   },
 
-  devtool: 'inline-source-map',
-
-  devServer: {
-    contentBase: path.join(__dirname, 'dist'),
-    hot: true,
-  },
-
   module: {
     rules: [
-      // --- css ---
       {
         test: /\.css$/,
-        // use extract text plugin in prod only
-        use: process.env.NODE_ENV === 'production'
-          ? ExtractTextWebpackPlugin.extract({
-              fallback: 'style-loader',
-              use: ['css-loader?modules'],
-            })
-          : ['style-loader', 'css-loader?modules'],
+        use: ['style-loader', 'css-loader?modules'],
       },
-      // --- images ---
       {
         test: /\.(png|svg|jpg|gif)$/,
         use: ['file-loader'],
       },
-      // --- fonts ---
       {
         test: /\.(woff|woff2|eot|ttf|otf)$/,
         use: ['file-loader'],
@@ -50,10 +38,50 @@ module.exports = {
   },
 
   plugins: [
-    new webpack.HotModuleReplacementPlugin(),
-    new CleanWebpackPlugin(['dist']),
     new HtmlWebpackPlugin({ title: 'GSW Theme Development' }),
-    new ExtractTextWebpackPlugin({ filename: '[name].css' }),
-    new UglifyJSPlugin(),
   ],
 };
+
+// development webpack config
+if (TARGET === 'start') {
+  module.exports = merge.smart(common, {
+    devtool: 'inline-source-map',
+
+    devServer: {
+      contentBase: path.join(__dirname, 'dist'),
+      hot: true,
+    },
+
+    plugins: [
+      new webpack.HotModuleReplacementPlugin(),
+    ],
+  });
+}
+
+// production webpack config
+if (TARGET === 'build') {
+  module.exports = merge.smart(common, {
+    devtool: 'source-map',
+
+    module: {
+      rules: [
+        {
+          test: /\.css$/,
+          use: ExtractTextWebpackPlugin.extract({
+            fallback: 'style-loader',
+            use: ['css-loader?modules'],
+          }),
+        },
+      ],
+    },
+
+    plugins: [
+      new CleanWebpackPlugin(['dist']),
+      new ExtractTextWebpackPlugin({ filename: '[name].css' }),
+      new UglifyJSPlugin({ sourceMap: true }),
+      new webpack.DefinePlugin({
+        'process.env.NODE_ENV': JSON.stringify('production')
+      })
+    ],
+  });
+}
